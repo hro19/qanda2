e0f309e637bd9b529130e95e067a0576b8a2de90
hori19 <fourbasic19@gmail.com>
2023-02-27 19:06:41 +0900

    【開発】エラー文を日本語化

--- config/application.rb
+++ config/application.rb
@@ -9,8 +9,12 @@ Bundler.require(*Rails.groups)
 module Qanda2
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
 
+    config.i18n.default_locale = :ja
+    config.time_zone = "Tokyo"
+    config.active_record.default_timezone = :local
+
+    config.load_defaults 7.0
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
