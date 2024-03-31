c45b2ac235e9ae63f5975f341e00ef66ec0b6ae2
hro19 <fourbasic19@gmail.com>
2024-03-31 20:11:35 +0900

    【rubocop】FrozenStringLiteralComment、Style/Documentationを無効化

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -4,12 +4,10 @@ class SearchesController < ApplicationController
 
     if @range == 'Question'
       @questions = Question.looks(params[:search], params[:word])
-      # p @questions
-      render '/searches/search_result'
     else
       @answers = Answer.looks(params[:search], params[:word])
-      # p @answers
-      render '/searches/search_result'
     end
+
+    render '/searches/search_result'
   end
 end
