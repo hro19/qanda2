7742e85bee43aa33554ad766c44dc81303e346e0
hro19 <fourbasic19@gmail.com>
2024-03-31 19:51:17 +0900

    Answerモデルの検索方法を改善

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,18 +1,26 @@
+
+# This class represents an answer to a question.
+#
+# It provides a method to perform different types of searches on the answer content.
+#
+# @param [String] search The type of search to perform.
+# @param [String] word The word to search for.
+# @return [ActiveRecord::Relation] The collection of answers that match the search criteria.
 class Answer < ApplicationRecord
   belongs_to :question
 
   # 検索方法分岐
   def self.looks(search, word)
-    @answers = if search == 'perfect_match'
-                 Answer.where('content LIKE?', "#{word}")
-               elsif search == 'forward_match'
-                 Answer.where('content LIKE?', "#{word}%")
-               elsif search == 'backward_match'
-                 Answer.where('content LIKE?', "%#{word}")
-               elsif search == 'partial_match'
-                 Answer.where('content LIKE?', "%#{word}%")
+    @answers = case search
+               when 'perfect_match'
+                 Answer.where('content LIKE ?', word.to_s)
+               when 'forward_match'
+                 Answer.where('content LIKE ?', "#{word}%")
+               when 'backward_match'
+                 Answer.where('content LIKE ?', "%#{word}")
+               when 'partial_match'
+                 Answer.where('content LIKE ?', "%#{word}%")
                else
                  Answer.all
                end
   end
-end
